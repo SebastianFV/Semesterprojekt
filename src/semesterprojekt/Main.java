2ce6f9056d65c695b0657e72801246d6e8362023
Squid <Squid@LAPTOP-NOS7UNAK.eduroam.wireless.dtu.dk>
2018-04-23 13:59:55 +0200

    Ændret navn på main til Main

---
+++ src/semesterprojekt/Main.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semesterprojekt;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Squid
+ */
+public class Main 
+{
+    static int finalPrice;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        Display display = new Display();        
+        Overview overview = new Overview();
+        GUI gui = new GUI();
+        gui.display = display;
+        gui.initialize();
+        JFrame vindue = new JFrame("Pet shop");
+        vindue.add(gui);
+        vindue.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        vindue.setSize(1000, 700);
+        vindue.setVisible(true);
+        
+        while (true)
+        {
+        }
+//        Carnivore tiger = new Carnivore(3, 30, "Tiger", 50, 10);
+//	while (tiger.getIsAlive())
+//	{
+//            System.out.println(tiger);
+//            tiger.birthday();
+//	}
+    }
+    
+}
